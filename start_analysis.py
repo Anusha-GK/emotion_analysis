3789927b449b36639decb27349ba2ea394f564b6
Anusha G Kademane <45038797+Anusha-GK@users.noreply.github.com>
2022-09-26 22:12:09 +0200

    Update start_analysis.py

--- start_analysis.py
+++ start_analysis.py
@@ -2,7 +2,7 @@ import json
 import boto3
 import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
+bucket = '<myBucket>'
 
 def lambda_handler(event, context):
     rekognition = boto3.client('rekognition', 'us-east-1')
@@ -11,13 +11,13 @@ def lambda_handler(event, context):
     FaceAttributes="ALL",
     Video={
         'S3Object': {
-            'Bucket': 'bucket-543425021727',
+            'Bucket': bucket,
             'Name': 'test2.mp4'
         }
     },
     NotificationChannel= { 
-      "SNSTopicArn": "arn:aws:sns:us-east-1:543425021727:AmazonRekognition_test",
-      "RoleArn": "arn:aws:iam::543425021727:role/RekognitionSNSRole"
+      "SNSTopicArn": "<AmazonRekognitionARN>",
+      "RoleArn": "<RekognitionSNSRole>"
   }
     
     )
